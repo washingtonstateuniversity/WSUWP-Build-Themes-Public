efab7bd91305e6a2b0fff007f0342efed2499c0b
jeremyfelt <jeremy.felt@gmail.com>
2014-06-03 11:35:10 -0700

    Remove the spine parent theme from the public themes repo
    This will be deployed via tag instead

--- spine/README.md
+++
@@ -1,4 +0,0 @@
-WSUWP Spine Parent Theme
-========================
-
-Provides the [spine and skeleton framework](https://github.com/washingtonstateuniversity/wsu-spine) for the WSU Web in WordPress
--- spine/inc/builder/core/templates/stage-header.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package Make
- */
-
-global $ttfmake_sections;
-?>
-
-<div class="ttfmake-stage<?php if ( empty( $ttfmake_sections ) ) echo ' ttfmake-stage-closed'?>" id="ttfmake-stage">
